28a948205195525b2530453bff2ad985f82914fc
chucoyos <manzanillo91@hotmail.com>
2020-06-15 05:21:00 -0500

    Recursos REST Formularios agregados

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,48 @@
 class BooksController < ApplicationController
     def index
-       @books = ['Primer Libro', 'Segundo Libro', 'Tercer Libro']
+       @books = Book.all
+    end
+
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.new(book_params)
+        if @book.save
+            redirect_to @book
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    def update
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+            redirect_to @book
+            else
+                render :edit
+        end
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+
+        redirect_to books_path, notice: "El libro ha sido eliminado exitosamente"
+    end
+
+    private
+    def book_params
+        params.require(:book).permit(:title, :author, :description, :publication_date,
+        :publication_date, :image_url, :price)
     end
 end
